755dddecc8dadb2ed43a09ae0f7df84846a618c6
Greg Oseid <3221311+goseid@users.noreply.github.com>
2021-04-29 20:23:38 -0500

    feat: query NASA, log response and return to client

--- src/server/index.js
+++ src/server/index.js
@@ -13,16 +13,39 @@ const port = 3000;
 app.use("/", express.static(path.join(__dirname, "../public")));
 
 // your API calls
-app.get("/rovers", (req, res) => {
+app.get("/rovers", async (req, res) => {
   if (req.query.rover) {
     const rover = req.query.rover;
+
     console.log(`/rovers was queried for ${rover}`);
-    res.send({ result: `Server received query received for ${rover}` });
+    //res.send({ result: `Server received query received for ${rover}` });
+    let resp = await fetch(
+      getEndPoint(rover) + process.env.API_KEY
+    ).then((res) => res.json());
+    console.log("NASA responded with:");
+    console.dir(resp);
+    res.send(resp);
   } else {
     console.dir(req);
-    res.send({ text: "Server received unexpected query" });
+    res.send({ text: "Server received unexpected query at /rovers." });
   }
 });
+
+getEndPoint = (rover) => {
+  const root = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
+
+  // Request sol 2190 since latest_photos for Spirit returns only 2 photos
+  // https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=2190&api_key={{NASA_API_KEY}}
+  if (rover === "Spirit") return root + "spirit/photos?sol=2190&api_key=";
+
+  // Request sol 5105 since latest_photos for Opportunity returns only 1 photo
+  // https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=5105&api_key={{NASA_API_KEY}}
+  if (rover === "Opportunity")
+    return root + "opportunity/photos?sol=5105&api_key=";
+
+  // https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key={{NASA_API_KEY}}
+  return root + rover.toLowerCase() + "/latest_photos?api_key=";
+};
 
 // example API call
 app.get("/apod", async (req, res) => {
